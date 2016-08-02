3f1b177fee74b7bf6d48806d1582bbb988b5da80
Michel Frechette <mifrec@hotmail.com>
2016-08-02 07:50:07 -0400

    fouth commit

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,5 @@
 puts "What is your name?"
+name = gets.chomp.capitalize
+puts "Hi #{name}, how old are ?"
+age = gets.chomp
+puts "If I'm right, #{name} was born in #{2016-age.to_i}, which means #{name} is #{age} years of age"
